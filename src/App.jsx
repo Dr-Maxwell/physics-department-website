2948a542bced4340b59a9759d1afa76e1cd6f1f7
samuel michael <samuelbodun4real@gmail.com>
2023-05-29 08:44:00 +0100

    committed

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
@@ -6,21 +8,19 @@ import "./Components/MyNav/Nav.css";
 import Nav from "./Components/Nav";
 import Home from "./pages/Home/Home";
 import LevelCourse from "./pages/LevelCourse/LevelCourse";
-import { AuthContextProvider } from "./contexts/AuthContext";
 
 // import routes from "./routes";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-      <div className="App">
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="levels/:level" element={<LevelCourse />} />
-        </Routes>
-      </div>
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="levels/:level" element={<LevelCourse />} />
+      </Routes>
+    </div>
   );
 }
-
 export default App;
