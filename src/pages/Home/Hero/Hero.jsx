f36b0ca3cce58a1a628e88ad5fb9a842efc3efed
samuel michael <samuelbodun4real@gmail.com>
2023-05-29 10:27:01 +0100

    uppdated

--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -1,8 +1,9 @@
+/** @format */
+
 import React from "react";
 import "./Hero.css";
 import HomeImg from "./background.jpg";
-import Typewriter from 'typewriter-effect';
-
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
   return (
@@ -10,17 +11,20 @@ const Hero = () => {
       {" "}
       <img src={HomeImg} alt="" />
       <div className="Home-text">
-        <h1><Typewriter
-  onInit={(typewriter) => {
-    typewriter.typeString('Welcome To Department Of Physics.')
-    .deleteAll()
-    typewriter.typeString('Exploring The Universe And Its Energy ...')
-      .pauseFor(1500)
-      // onInit()
-      .start();
-      
-  }}
-/></h1>
+        <h3>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Welcome To Department Of Physics.")
+                .deleteAll();
+              typewriter
+                .typeString("Exploring The Universe And Its Energy ...")
+                .pauseFor(1500)
+                // onInit()
+                .start();
+            }}
+          />
+        </h3>
 
         <a href="#about">
           <button>LEARN MORE</button>
